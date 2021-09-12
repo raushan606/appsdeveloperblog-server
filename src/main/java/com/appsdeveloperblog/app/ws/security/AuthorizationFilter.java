f717a3c781ce8572c9461e707ba1281088c53001
raushan606 <raushankumar606@gmail.com>
2021-09-12 12:25:26 +0530

    restStatelessapi

--- src/main/java/com/appsdeveloperblog/app/ws/security/AuthorizationFilter.java
+++ src/main/java/com/appsdeveloperblog/app/ws/security/AuthorizationFilter.java
@@ -17,39 +17,43 @@ import io.jsonwebtoken.Jwts;
 
 public class AuthorizationFilter extends BasicAuthenticationFilter {
 
-	public AuthorizationFilter(AuthenticationManager authenticationManager) {
-		super(authenticationManager);
+	public AuthorizationFilter(AuthenticationManager authManager) {
+		super(authManager);
 	}
 
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+	protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
 
-		String header = request.getHeader(SecurityConstants.HEADER_STRING);
+		String header = req.getHeader(SecurityConstants.HEADER_STRING);
 
-		if (header == null || header.startsWith(SecurityConstants.TOKEN_PREFIX)) {
-			chain.doFilter(request, response);
+		if (header == null || !header.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+			chain.doFilter(req, res);
 			return;
 		}
 
-		UsernamePasswordAuthenticationToken authenticationFilter = getAuthentication(request);
-		SecurityContextHolder.getContext().setAuthentication(authenticationFilter);
-		chain.doFilter(request, response);
-
+		UsernamePasswordAuthenticationToken authentication = getAuthentication(req);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		chain.doFilter(req, res);
 	}
 
 	private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
-
 		String token = request.getHeader(SecurityConstants.HEADER_STRING);
 
 		if (token != null) {
+
 			token = token.replace(SecurityConstants.TOKEN_PREFIX, "");
+
 			String user = Jwts.parser().setSigningKey(SecurityConstants.TOKEN_SECRET).parseClaimsJws(token).getBody()
 					.getSubject();
-			if (user != null)
+
+			if (user != null) {
 				return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+			}
+
 			return null;
 		}
+
 		return null;
 	}
 
